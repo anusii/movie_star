efb5cda24ced1c861b03d46d08e4fe9990e1835a
atangster <ashley.t1248@gmail.com>
2025-06-24 11:04:47 +1000

    improve code format

--- lib/providers/database_provider.dart
+++ lib/providers/database_provider.dart
@@ -1,10 +1,27 @@
 /// Database provider for the Movie Star application.
 ///
-/// This provider manages the AppDatabase instance and provides access
-/// to the database throughout the application using Riverpod.
+// Time-stamp: <Thursday 2025-04-10 11:47:48 +1000 Graham Williams>
 ///
 /// Copyright (C) 2025, Software Innovation Institute, ANU.
+///
 /// Licensed under the GNU General Public License, Version 3 (the "License").
+///
+/// License: https://www.gnu.org/licenses/gpl-3.0.en.html.
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+///
+/// Authors: Ashley Tang
 
 library;
 
@@ -12,15 +29,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:moviestar/database/app_database.dart';
 
-/// Provider for the main application database
+/// Provider for the main application database.
 ///
 /// This creates a singleton instance of AppDatabase that can be accessed
 /// throughout the application. The database will be automatically closed
 /// when the provider is disposed.
+
 final databaseProvider = Provider<AppDatabase>((ref) {
   final database = AppDatabase();
 
-  // Ensure the database is closed when the provider is disposed
+  // Ensure the database is closed when the provider is disposed.
+
   ref.onDispose(() {
     database.close();
   });
